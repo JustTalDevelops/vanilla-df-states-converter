f8ffea8f1ce5c63f9d86d471a08dfd275dc1a0a4
JustTal <justtal@protonmail.com>
2023-04-20 10:21:18 -0500

    vanilla-df-states-converter: Various fixes.

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/JustTalDevelops/vanilla-df-states-converter
 
-go 1.16
+go 1.18
 
 require (
-	github.com/df-mc/dragonfly v0.0.4
+	github.com/df-mc/dragonfly v0.8.3
 	github.com/sandertv/gophertunnel v1.10.3
 )
--- main.go
+++ main.go
@@ -3,68 +3,62 @@ package main
 import (
 	"bytes"
 	"compress/gzip"
-	"fmt"
-	"github.com/df-mc/dragonfly/dragonfly/world"
 	"github.com/sandertv/gophertunnel/minecraft/nbt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 func main() {
-	// Read arguments and error if there are too little
-	args := os.Args[1:]
-	if len(args) < 2 {
-		fmt.Println("Usage: vanilla-df-states-converter.exe vanilla-pallete.nbt output.nbt")
-		return
-	}
+	inputFile := "block_palette.nbt"
+	outputFile := "block_states.nbt"
 
-	// Mark the input and output file names
-	inputFile := args[0]
-	outputFile := args[1]
-
-	// Load the contents of the input file
 	b, err := os.ReadFile(inputFile)
 	if err != nil {
 		panic(err)
 	}
 
-	// The vanilla palette that the vanilla states are unmarshalled into temporarily.
 	var vanillaPalette struct {
-		Blocks []world.BlockState `nbt:"blocks"`
+		Blocks []struct {
+			Name       string         `nbt:"name"`
+			Properties map[string]any `nbt:"states"`
+			Version    int32          `nbt:"version"`
+			NameHash   int64          `nbt:"name_hash"`
+		} `nbt:"blocks"`
 	}
 
-	// The vanilla states are gzip compressed, so before we can unmarshal them, we decompress them.
 	gr, err := gzip.NewReader(bytes.NewBuffer(b))
 	if err == nil {
-		// Read the bytes from the IO reader
-		b, err = ioutil.ReadAll(gr)
+		b, err = io.ReadAll(gr)
 		if err != nil {
 			panic(err)
 		}
-
-		defer gr.Close()
+		_ = gr.Close()
 	}
 
-	// Unmarshal the bytes into the vanilla palette with BigEndian encoding.
 	err = nbt.UnmarshalEncoding(b, &vanillaPalette, nbt.BigEndian)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create a new encoder with an empty byte buffer
 	buf := new(bytes.Buffer)
 	e := nbt.NewEncoder(buf)
 
-	// Encode every block state and add it to the buffer
 	for _, s := range vanillaPalette.Blocks {
-		err := e.Encode(&s)
+		err := e.Encode(struct {
+			Name       string         `nbt:"name"`
+			Properties map[string]any `nbt:"states"`
+			Version    int32          `nbt:"version"`
+		}{
+			Name:       s.Name,
+			Properties: s.Properties,
+			Version:    s.Version,
+		})
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	// Write the output bytes to a file
-	err = ioutil.WriteFile(outputFile, buf.Bytes(), 0777)
+	err = os.WriteFile(outputFile, buf.Bytes(), 0777)
 	if err != nil {
 		panic(err)
 	}
